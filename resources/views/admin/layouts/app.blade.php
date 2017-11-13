e26589d042cae33358ee9f2e12ae9abcbb61142b
Javier Ocampo <ferroxido@gmail.com>
2017-11-13 11:42:28 -0300

    SB admin dashboard

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'School App') }}</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/timeline.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+
+</head>
+<body>
+<div id="app">
+    @yield('body')
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/admin.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,73 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/', [
+        'uses' => 'HomeController@index',
+        'as' => 'home'
+    ]);
+
+    Route::get('/home', [
+        'uses' => 'HomeController@index',
+        'as' => 'home'
+    ]);
+});
+
+Route::get('/charts', function () {
+    return View::make('admin.charts');
+});
+
+Route::get('/tables', function () {
+    return View::make('admin.table');
+});
+
+Route::get('/forms', function () {
+    return View::make('admin.form');
+});
+
+Route::get('/grid', function () {
+    return View::make('admin.grid');
+});
+
+Route::get('/buttons', function () {
+    return View::make('admin.buttons');
+});
+
+Route::get('/icons', function () {
+    return View::make('admin.icons');
+});
+
+Route::get('/panels', function () {
+    return View::make('admin.panel');
+});
+
+Route::get('/typography', function () {
+    return View::make('admin.typography');
+});
+
+Route::get('/notifications', function () {
+    return View::make('admin.notifications');
+});
+
+Route::get('/blank', function () {
+    return View::make('admin.blank');
+});
+
+Route::get('/documentation', function () {
+    return View::make('admin.documentation');
+});
+
+Route::get('/stats', function() {
+   return View::make('admin.stats');
+});
+
+Route::get('/progressbars', function() {
+    return View::make('admin.progressbars');
+});
+
+Route::get('/collapse', function() {
+    return View::make('admin.collapse');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
